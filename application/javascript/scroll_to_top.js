3ef648145037c4a0cac64242e2dcce228097c903
janoscarfilip <og200029@gmail.com>
2024-07-11 14:49:38 +0200

    Added scroll-to-top & updated page-refresh

---
+++ application/javascript/scroll_to_top.js
@@ -0,0 +1,8 @@
+// Get the button:
+let mybutton = document.getElementById("refresh_page_button");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
